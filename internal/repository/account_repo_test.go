e1e79b3bf7834573f4dd98216b6ede020873910f
jesslourenco <70455379+jesslourenco@users.noreply.github.com>
2024-06-06 20:27:46 -0300

    create

--- internal/repository/account_repo_test.go
+++ internal/repository/account_repo_test.go
@@ -8,6 +8,92 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestCreateAccount(t *testing.T) {
+	id := "0003"
+	idGenerator := func() string {
+		return id
+	}
+
+	type args struct {
+		ctx      context.Context
+		name     string
+		lastname string
+		data     map[string]models.Account
+	}
+
+	var scenarios = map[string]struct {
+		given   args
+		want    models.Account
+		wantErr error
+	}{
+		"happy-path": {
+			given: args{
+				ctx:      context.Background(),
+				name:     "Caio",
+				lastname: "Henrique",
+				data: map[string]models.Account{
+					"0001": {
+						AccountId: "0001",
+						Name:      "Shankar",
+						LastName:  "Nakai",
+					},
+					"0002": {
+						AccountId: "0002",
+						Name:      "Jessica",
+						LastName:  "Lourenco",
+					}},
+			},
+
+			want: models.Account{
+				AccountId: id,
+				Name:      "Caio",
+				LastName:  "Henrique",
+			},
+			wantErr: nil,
+		},
+		"missing params": {
+			given: args{
+				ctx:      context.Background(),
+				name:     "",
+				lastname: "Henrique",
+				data: map[string]models.Account{
+					"0001": {
+						AccountId: "0001",
+						Name:      "Shankar",
+						LastName:  "Nakai",
+					},
+					"0002": {
+						AccountId: "0002",
+						Name:      "Jessica",
+						LastName:  "Lourenco",
+					}},
+			},
+
+			want:    models.Account{},
+			wantErr: ErrMissingParams,
+		},
+	}
+
+	for name, tcase := range scenarios {
+		tcase := tcase
+		t.Run(name, func(t *testing.T) {
+			repo := setup(t, tcase.given.data, idGenerator)
+
+			id, err := repo.Create(tcase.given.ctx, tcase.given.name, tcase.given.lastname)
+
+			if tcase.wantErr == nil {
+				assert.NoError(t, err)
+				acc, err := repo.FindOne(tcase.given.ctx, id)
+				assert.NoError(t, err)
+				assert.Equal(t, tcase.want, acc)
+			} else {
+				assert.EqualError(t, err, tcase.wantErr.Error())
+			}
+
+		})
+	}
+}
+
 func TestFindAll(t *testing.T) {
 	type args struct {
 		ctx  context.Context
@@ -62,7 +148,7 @@ func TestFindAll(t *testing.T) {
 	for name, tcase := range scenarios {
 		tcase := tcase
 		t.Run(name, func(t *testing.T) {
-			repo := setup(t, tcase.given.data)
+			repo := setup(t, tcase.given.data, nil)
 
 			result, err := repo.FindAll(tcase.given.ctx)
 
@@ -140,7 +226,7 @@ func TestFindOne(t *testing.T) {
 	for name, tcase := range scenarios {
 		tcase := tcase
 		t.Run(name, func(t *testing.T) {
-			repo := setup(t, tcase.given.data)
+			repo := setup(t, tcase.given.data, nil)
 
 			result, err := repo.FindOne(tcase.given.ctx, tcase.given.id)
 
@@ -157,8 +243,9 @@ func TestFindOne(t *testing.T) {
 
 }
 
-func setup(_ *testing.T, initialData map[string]models.Account) *accountRepoImpl {
+func setup(_ *testing.T, initialData map[string]models.Account, idGenerator func() string) *accountRepoImpl {
 	repo := NewAccountRepo()
 	repo.accounts = initialData
+	repo.idGenerator = idGenerator
 	return repo
 }
