7080e8f7ea71b8c2a7b1796ba5fa8661e3cb99f4
jesslourenco <70455379+jesslourenco@users.noreply.github.com>
2024-06-29 20:19:09 -0300

    Add handler structure

--- internal/router.go
+++ internal/router.go
@@ -2,13 +2,11 @@ package internal
 
 import "github.com/julienschmidt/httprouter"
 
-func Router(routes []Route) *httprouter.Router {
+func Router(registers ...HandlerRegister) *httprouter.Router {
 	router := httprouter.New()
 
-	for _, route := range routes {
-		var handle httprouter.Handle = route.HandlerFunc
-
-		router.Handle(route.Method, route.Path, handle)
+	for _, r := range registers {
+		r.Register(router)
 	}
 	return router
 }
